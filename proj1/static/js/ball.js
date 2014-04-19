ff1defe45ce3204167e4af8efda60e06cb8acefd
kriyazhao <t.zhao2011@gmail.com>
2014-04-19 14:06:59 -0400

    init

---
+++ proj1/static/js/ball.js
@@ -0,0 +1,208 @@
+var click_stat=false;
+
+//Code to move bar
+function bindMouseToPad(){
+	$(document).mousemove(function(m){
+		setTimeout(hideHint,2000);
+		$("#body").css('cursor', 'none');
+		if(m.pageX>10&&m.pageX<window.innerWidth-180)
+		{
+			$(".my_pad").css({"left":m.pageX-50, "top":m.pageY-10});
+		}
+	});
+}
+		
+var ballTimer = window.setInterval(function() {
+	if(judgeBallValid()==1){
+		 window.clearInterval(ballTimer)
+		 if(confirm("Out of upper bound! Press to Continue"))
+		 {
+			 continueGame(1);
+			 popup("auto-continue in 3 seconds");
+		 }else{
+			 popup("Game over!");
+			 emptyPairPlayers();
+		 }
+		 return;
+	}else if(judgeBallValid()==2){
+		 window.clearInterval(ballTimer)
+		 if(confirm("Out of lower bound!Press to Continue"))
+		 {
+			 continueGame(2);
+			 popup("auto-continue in 3 seconds");
+		 }else{
+			 popup("Game over!");
+			 emptyPairPlayers();
+		 }
+		 return;
+	}
+	
+	if(click_stat==true){
+	
+		var p = $(".ball");
+		var position = p.position();
+		var status=getStatus();
+		
+		if(status==1)
+		{
+			var tadd=getTopOffset();
+			var ladd=getLeftOffset();
+			$(".ball").css({"top":position.top+tadd,"left":position.left+ladd});
+		}
+		else if(status==2)//ball status changed by rival
+		{
+			ballStatusChanged = false;
+			if(ballStatus==null)
+				return;
+			else{
+				getLeftOffset.lOffset = ballStatus.x;
+				getTopOffset.tOffset = ballStatus.y;
+			}
+			
+		}
+	}
+},40);
+
+
+function getLeftOffset()
+{
+	var lOffset=0;
+	var pball = $(".ball");
+	var pad1 = $(".my_pad");
+	var bar1_position = pad1.position();
+	var pad2 = $(".rival_pad");
+	var bar2_position = pad2.position();
+	var ball_position = pball.position();
+	if(Math.abs(ball_position.top-bar1_position.top)<=5||
+			Math.abs(ball_position.top-bar2_position.top)<=5)//when ball near the pad
+	{
+		if((ball_position.left-bar1_position.left>=-10 && 
+				ball_position.left-bar1_position.left<=65)||
+			(ball_position.left-bar2_position.left>=-10 && 
+				ball_position.left-bar2_position.left<=65))//left diff [10, 100]
+		{
+			lOffset=-2; 
+		}
+		if((ball_position.left-bar1_position.left<=140 && 
+				ball_position.left-bar1_position.left>65)||
+			(ball_position.left-bar2_position.left<=140 && 
+					ball_position.left-bar2_position.left>65) )//left diff [90, 190]
+		{
+			lOffset=2;		
+		}
+	}
+
+	if(lOffset==0){lOffset=getLeftOffset.lOffset;}//keep the same
+	if(ball_position.left<=$(".play_ground").position().left|| //the two sides of table
+			ball_position.left>=$(".play_ground").position().left+500){
+		lOffset=-lOffset;
+	}
+		
+
+	getLeftOffset.lOffset=lOffset;
+	return lOffset;
+}
+
+function getTopOffset()
+{
+	var tOffset=0;
+	var pball = $(".ball");
+	var pad1 = $(".my_pad");
+	var ball_position = pball.position();
+	var bar1_position = pad1.position();
+	var pad2 = $(".rival_pad");
+	var bar2_position = pad2.position();
+	
+	if( (ball_position.left-bar1_position.left>=-10 && 
+		ball_position.left-bar1_position.left<=140 &&
+		ball_position.top-bar1_position.top>=0 && 
+		ball_position.top-bar1_position.top<=10)||
+		(ball_position.left-bar2_position.left>=-10 && 
+		ball_position.left-bar2_position.left<=140 &&
+		ball_position.top-bar2_position.top>=0 && 
+		ball_position.top-bar2_position.top<=10)) {
+		tOffset=5;
+		/*changeBallStatus(ball_position.left,ball_position.top,0,tOffset);
+		TOPoffset = 5;*/
+	}
+		
+	if( (ball_position.left-bar1_position.left>=-10 && 
+		ball_position.left-bar1_position.left<=140 &&
+		ball_position.top-bar1_position.top>=-10 && 
+		ball_position.top-bar1_position.top<=0)||
+		(ball_position.left-bar2_position.left>=-10 && 
+		ball_position.left-bar2_position.left<=140 &&
+		ball_position.top-bar2_position.top>=-10 && 
+		ball_position.top-bar2_position.top<=0)) {
+		tOffset=-5;
+		/*changeBallStatus(ball_position.left,ball_position.top,0,tOffset);
+		TOPoffset = -5;*/
+	}
+	
+	if(tOffset==0){
+		tOffset=getTopOffset.tOffset;
+	}
+	getTopOffset.tOffset=tOffset;
+	
+	return tOffset;
+}
+
+function getStatus()
+{
+	/*if(ballStatusChanged)
+		return 2;*/
+	
+	/*var stat;
+	var pball = $(".ball");
+	var pad = $(".my_pad");
+	var bar_position = pad.position();
+	var ball_position = pball.position();
+	if(ball_position.top>=bar_position.top-20){
+		//if(ball_position.left+22>=bar_position.left && ball_position.left<=bar_position.left+200)
+			stat=1;//ball move on
+		//else
+		//	stat=0;//game over
+	}
+	else stat=0;
+	
+	/*if(stat!=0&&($("#block_blue1").length==0)&&($("#block_blue2").length==0)
+		&&($("#block_red1").length==0)&&($("#block_red2").length==0)
+		&&($("#block_pink1").length==0)&&($("#block_pink2").length==0))
+	stat=2;*/
+	
+	return 1;
+}
+
+
+function judgeBallValid(){
+	var pball = $(".ball");
+	var ball_position = pball.position();
+	if(ball_position.top<60){//out of upper bound
+		addScoreForMe()
+		return 1;
+	} 
+	else if(ball_position.top>600+80)// out of lower bound
+	{
+		addScoreForRival()
+		return 2;
+	}
+	else
+		return 0;
+}
+
+function continueGame(flag){
+	if(flat==1){//upper, give ball to upper person
+		$(".ball").css("left",400);
+		$(".ball").css("top",200);
+		setTimeout(function(){
+			getTopOffset.tOffset = 5;
+		},3000)
+	}else if(flat==2){
+		$(".ball").css("left",400);
+		$(".ball").css("top",600);
+		setTimeout(function(){
+			getTopOffset.tOffset = -5;
+		},3000)
+	}
+}
+
